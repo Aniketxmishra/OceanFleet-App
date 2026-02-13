9b6409c4ae8e6172ae29dff5479f32227c65a788
Aniket Mishra <aniketmishrarr@gmail.com>
2026-02-13 12:32:35 +0530

    [Aniket] Added: feat [uc2] : add VesselUtil with list-based storage

---
+++ src/main/java/com/oceanfleet/util/VesselUtil.java
@@ -0,0 +1,29 @@
+package com.oceanfleet.util;
+
+import com.oceanfleet.model.Vessel;
+import java.util.ArrayList;
+import java.util.List;
+
+public class VesselUtil {
+    private List<Vessel> vesselList;
+
+    // Constructor
+    public VesselUtil() {
+        this.vesselList = new ArrayList<>();
+    }
+
+    // Getter
+    public List<Vessel> getVesselList() {
+        return vesselList;
+    }
+
+    // Setter
+    public void setVesselList(List<Vessel> vesselList) {
+        this.vesselList = vesselList;
+    }
+
+    // UC2: Add Vessel performance details
+    public void addVesselPerformance(Vessel vessel) {
+        vesselList.add(vessel);
+    }
+}
